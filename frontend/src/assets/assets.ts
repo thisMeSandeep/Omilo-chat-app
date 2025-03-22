e9ac52c01387b96455da434c0392e0fcf69cba40
Sandeep Singh <snayal50@gmail.com>
2025-03-22 11:44:58 +0530

    assets prepared

---
+++ frontend/src/assets/assets.ts
@@ -0,0 +1,47 @@
+import chat from "../assets/chat.svg";
+import clock from "../assets/clock.svg";
+import download from "../assets/download.svg";
+import emoji from "../assets/emoji.svg";
+import eyeCrossed from "../assets/eye-crossed.svg";
+import eye from "../assets/eye.svg";
+import fileImage from "../assets/file-image.svg";
+import fileUpload from "../assets/file-upload.svg";
+import menuDotsVertical from "../assets/menu-dots-vertical.svg";
+import menuDotsHorizontal from "../assets/menu-dots.svg";
+import moon from "../assets/moon.svg";
+import omilo from "../assets/omilo.png";
+import pencil from "../assets/pencil.svg";
+import picture from "../assets/picture.svg";
+import save from "../assets/save.svg";
+import search from "../assets/search.svg";
+import send from "../assets/send.svg";
+import settings from "../assets/settings.svg";
+import signOut from "../assets/sign-out-alt.svg";
+import sun from "../assets/sun.svg";
+import trash from "../assets/trash.svg";
+import user from "../assets/user.svg";
+
+export const assets = {
+  chat,
+  clock,
+  download,
+  emoji,
+  eyeCrossed,
+  eye,
+  fileImage,
+  fileUpload,
+  menuDotsHorizontal,
+  menuDotsVertical,
+  moon,
+  omilo,
+  pencil,
+  picture,
+  save,
+  search,
+  send,
+  settings,
+  signOut,
+  sun,
+  trash,
+  user,
+};
