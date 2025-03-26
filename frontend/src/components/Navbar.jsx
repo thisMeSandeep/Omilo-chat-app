c2764df0a58bb97594da49c5318446deff1b99f9
Sandeep Singh <snayal50@gmail.com>
2025-03-26 12:00:49 +0530

    dashboard link added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import ThemeToggler from "./ui/ThemeToggler"
 import { useEffect, useState } from "react"
-import { MessageCircleDashed } from "lucide-react";
+import { LayoutDashboard, MessageCircleDashed } from "lucide-react";
 
 const Navbar = () => {
     const [scrolled, setScrolled] = useState(false);
@@ -26,7 +26,7 @@ const Navbar = () => {
                 {/* logo */}
                 <Link to="/" className='flex items-center gap-2'>
                     <div className='size-8 rounded-full bg-omilo-primary flex items-center justify-center p-1'>
-                        <MessageCircleDashed className="size-5 text-white"/>
+                        <MessageCircleDashed className="size-5 text-white" />
                     </div>
                     <span className="text-2xl font-bold logo text-glow">Omilo</span>
                 </Link>
@@ -39,6 +39,8 @@ const Navbar = () => {
                     <Link to='/login' className="hidden md:inline">
                         <button className="cursor-pointer text-sm tracking-tight px-5 py-2.5 rounded-md hover:border hover:border-omilo-primary hover:text-omilo-primary">Sign In</button>
                     </Link>
+
+                    <Link to='/dashboard'><LayoutDashboard className="size-5"/></Link>
 
                     <Link to='/signUp'>
                         <button className="cursor-pointer text-sm tracking-tight px-5 py-2.5 rounded-md bg-omilo-primary text-white">Get Started</button>
